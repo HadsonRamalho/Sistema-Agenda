c9ca1ad2e10bad04764421353f1b97a23c3e3211
HadsonRamalho <133451081+HadsonRamalho@users.noreply.github.com>
2024-02-27 13:23:06 -0300

    Atualizar o main.cpp

--- Sistema Agenda/Sistema Agenda/main.cpp
+++ Sistema Agenda/Sistema Agenda/main.cpp
@@ -7,163 +7,225 @@ using namespace std;
 
 class Conta {
 private:
-	string nome;
-	string email;
-	string senha;
-	string tipo_de_conta;
+    string nome;
+    string email;
+    string senha;
+    string tipo_de_conta;
+    bool primeiro_login;
 public:
-	Conta() {
-		this->nome = "default";
-		this->email = "default";
-		this->senha = "default";
-		this->tipo_de_conta = "undefined";
-	}
-	void adicionar_conta(string nome, string email, string senha);
-	string retorna_nome();
-	string retorna_email();
-	string retorna_senha();
-	string retorna_tipo_conta();
+    Conta() {
+        this->nome = "default";
+        this->email = "default";
+        this->senha = "default";
+        this->tipo_de_conta = "undefined";
+        this->primeiro_login = true;
+    }
+    void adicionar_conta(string nome, string email, string senha);
+    string retorna_nome();
+    string retorna_email();
+    string retorna_senha();
+    string retorna_tipo_conta();
 };
 
 string Conta::retorna_nome() {
-	return this->nome;
+    return this->nome;
 }
 
 string Conta::retorna_email() {
-	return this->email;
+    return this->email;
 }
 
 string Conta::retorna_senha() {
-	return this->senha;
+    return this->senha;
 }
 
 string Conta::retorna_tipo_conta() {
-	return this->tipo_de_conta;
+    return this->tipo_de_conta;
 }
 
 void Conta::adicionar_conta(string nome, string email, string senha) {
-	this->nome = nome;
-	this->email = email;
-	this->senha = senha;
-	this->tipo_de_conta = tipo_de_conta;
+    this->nome = nome;
+    this->email = email;
+    this->senha = senha;
+
 }
 
+bool email_valido(string email);
+
 Conta criar_conta() {
-	Conta Usuario_Temporario;
-	cout << "\tMENU DE CRIACAO DE CONTA" << endl
-		<< " | Insira seu nome: " << endl;
-	string nome_usuario;
-	cin >> nome_usuario;
-	cout << " | Insira seu email: " << endl;
-	string email_usuario;
-	cin >> email_usuario;
-	string senha_usuario_parte1;
-	string senha_usuario_parte2;
-	bool senha_valida = false;
-	do{
-		cout << " | Insira sua senha: " << endl;
-		cin >> senha_usuario_parte1;
-		cout << " | Confirme sua senha: " << endl;
-		cin >> senha_usuario_parte2;
-		if (senha_usuario_parte1.compare(senha_usuario_parte2) != 0) {
-			cerr << "Senhas diferentes. Digite novamente!" << endl;
-			senha_valida = false;
-		}
-		else {
-			senha_valida = true;
-		}
-	} while (!senha_valida);
-	string senha_usuario = senha_usuario_parte1;
-	Usuario_Temporario.adicionar_conta(nome_usuario, email_usuario, senha_usuario);
-	cout << " Sucesso na criacao da conta!" << endl
-		<< " | Nome: " << nome_usuario << endl
-		<< " | Email: " << email_usuario << endl;
-	return Usuario_Temporario;
+    Conta Usuario_Temporario;
+    cout << "\tMENU DE CRIACAO DE CONTA" << endl
+         << " | Insira seu nome: " << endl;
+    string nome_usuario;
+    cin >> nome_usuario;
+    cout << " | Insira seu email: " << endl;
+    string email_usuario;
+    cin >> email_usuario;
+    email_valido(email_usuario);
+    string senha_usuario_parte1;
+    string senha_usuario_parte2;
+    bool senha_valida = false;
+    do {
+        cout << " | Insira sua senha: " << endl;
+        cin >> senha_usuario_parte1;
+        cout << " | Confirme sua senha: " << endl;
+        cin >> senha_usuario_parte2;
+        if (senha_usuario_parte1.compare(senha_usuario_parte2) != 0) {
+            cerr << "Senhas diferentes. Digite novamente!" << endl;
+            senha_valida = false;
+        }
+        else {
+            senha_valida = true;
+        }
+    } while (!senha_valida);
+    string senha_usuario = senha_usuario_parte1;
+    Usuario_Temporario.adicionar_conta(nome_usuario, email_usuario, senha_usuario);
+    cout << " Sucesso na criacao da conta!" << endl
+         << " | Nome: " << nome_usuario << endl
+         << " | Email: " << email_usuario << endl;
+    return Usuario_Temporario;
 }
 
 void listar_contas(Conta Contas[], const int cadastros) {
-	for (int i = 0; i < cadastros; i++) {
-		cout << Contas[i].retorna_nome() << endl;
-		cout << Contas[i].retorna_email() << endl;
-	}
+    for (int i = 0; i < cadastros; i++) {
+        cout << "-------------------" << endl;
+        cout << Contas[i].retorna_nome() << endl;
+        cout << Contas[i].retorna_email() << endl;
+    }
 }
 
 void redefinir_senha_login() {
-	cout << "\tMENU DE REDEFINICAO DE SENHA" << endl
-		<< " | Digite seu email: " << endl;
-	string email_recuperacao;
-	cin >> email_recuperacao;
-	int codigo_verificacao[5];
-	cout << " | Codigo de verificacao: " << endl;
-	srand((unsigned)time(NULL));
-	for (int i = 0; i < 5; i++) {
-		codigo_verificacao[i] = 0 + (rand() % 10);
-		cout << codigo_verificacao[i];
-		if (i != 4) {
-			cout << " | ";
-		}
-	}
-	cout << " | Digite o codigo de verificao: ";
-	int codigo_usuario[5];
-	for (int i = 0; i < 5; i++) {
-		cin >> codigo_usuario[i];
-	}
-	bool codigo_valido = false;
-	for (int i = 0; i < 5; i++) {
-		if (codigo_usuario[i] != codigo_verificacao[i]) {
-			codigo_valido = false;
-			cerr << " | Codigo invalido" << endl;
-			break;
-		}
-		cout << " | Codigo valido! " << endl;
-	}
+    cout << "\tMENU DE REDEFINICAO DE SENHA" << endl
+         << " | Digite seu email: " << endl;
+    string email_recuperacao;
+    cin >> email_recuperacao;
+    int codigo_verificacao[5];
+    cout << " | Codigo de verificacao: " << endl;
+    srand((unsigned)time(NULL));
+    for (int i = 0; i < 5; i++) {
+        codigo_verificacao[i] = 0 + (rand() % 10);
+        cout << codigo_verificacao[i];
+        if (i != 4) {
+            cout << " | ";
+        }
+    }
+    cout << " | Digite o codigo de verificao: ";
+    int codigo_usuario[5];
+    for (int i = 0; i < 5; i++) {
+        cin >> codigo_usuario[i];
+    }
+    bool codigo_valido = false;
+    for (int i = 0; i < 5; i++) {
+        if (codigo_usuario[i] != codigo_verificacao[i]) {
+            codigo_valido = false;
+            cerr << " | Codigo invalido" << endl;
+            break;
+        }
+        cout << " | Codigo valido! " << endl;
+    }
+}
+
+int verifica_cadastro_email(Conta Contas[], int &cadastros, string email) {
+    for(int i = 0; i < cadastros; i++) {
+        if(Contas[i].retorna_email().compare(email) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+bool email_existe(Conta Contas[], int &cadastros, string email){
+	for(int i = 0; i < cadastros; i++){
+    	if(Contas[i].retorna_email().compare(email) == 0){
+        	return true;
+        }
+    }
+    return false;
+}
+
+bool email_valido(string email){
+	int indice = email.find("@gmail.com");
+    while(indice == -1){
+    	cerr << " | Email invalido. Digite novamente: ";
+        cin >> email;
+        indice = email.find("@gmail.com");
+    }    
+    return true;    
+}
+
+int verifica_cadastro_senha(Conta Contas[], int&cadastros, string senha) {
+    for(int i = 0; i < cadastros; i++) {
+        if(Contas[i].retorna_senha().compare(senha) == 0) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 void menu_login(Conta Contas[], int &cadastros) {
-	string opcao;
-	string email_login;
-	string senha_login;
-	Conta Conta_Temp;
-	do {
-		cout << "\tMENU DE LOGIN" << endl
-			<< " 1 | Tenho conta" << endl
-			<< " 2 | Nao tenho conta" << endl
-			<< " [DEV] 3 | Listar contas" << endl
-			<< " [DEV] 4 | Sair" << endl;
-		cin >> opcao;
-		switch (opcao[0]) {
-		case '1':
-			cout << " | Insira seu email: ";
-			cin >> email_login;
-			cout << " | Insira sua senha [-1 para 'Esqueci minha senha']: ";
-			cin >> senha_login;
-			if (senha_login.compare("-1") == 0) {
-				redefinir_senha_login();
-			}
-			break;
-		case '2':
-			Conta_Temp = criar_conta();
-			Contas[cadastros].adicionar_conta(Conta_Temp.retorna_nome(), Conta_Temp.retorna_email(), Conta_Temp.retorna_senha());
-			cadastros++;
-			break;
-		case '3':
-			listar_contas(Contas, cadastros);
-			break;
-		default:
-			cerr << "Opcao invalida. Saindo!" << endl;
-			opcao[0] = '-1';
-		}
+    string opcao;
+    string email_login;
+    string senha_login;
+    Conta Conta_Temp;
+    do {
+        cout << "\tMENU DE LOGIN" << endl
+             << " 1 | Tenho conta" << endl
+             << " 2 | Nao tenho conta" << endl
+             << " [DEV] 3 | Listar contas" << endl
+             << " [DEV] 4 | Sair" << endl;
+        cin >> opcao;
+        switch (opcao[0]) {
+        case '1':
+            cout << " | Insira seu email: ";
+            cin >> email_login;
+            email_valido(email_login);
+            while (!email_existe(Contas, cadastros, email_login)){
+            	cerr << " | Email incorreto! Digite novamente: ";
+                cin >> email_login;
+                email_valido(email_login);
+                email_existe(Contas, cadastros, email_login);
+            }
+                        cout << " | Insira sua senha [-1 para 'Esqueci minha senha']: ";
+            cin >> senha_login;
+            if (senha_login.compare("-1") == 0) {
+                redefinir_senha_login();
+            }
+            else {
+                if(verifica_cadastro_senha(Contas, cadastros, senha_login) != -1) {
+                    cout << " | Login realizado com sucesso!" << endl;
+                }
+                else {
+                   // while(verifica_cadastro_senha(Contas, cadastros, senha_login) != -1){
+                        cerr << " | Senha incorreta. Digite novamente: ";
+                        cin >> senha_login;
+                    
+                }
+            }
+            break;
+        case '2':
+            Conta_Temp = criar_conta();
+            Contas[cadastros].adicionar_conta(Conta_Temp.retorna_nome(), Conta_Temp.retorna_email(), Conta_Temp.retorna_senha());
+            cadastros++;
+            break;
+        case '3':
+            listar_contas(Contas, cadastros);
+            break;
+        default:
+            cerr << "Opcao invalida. Saindo!" << endl;
+            opcao[0] = '-1';
+        }
 
-	} while (opcao[0] == '4' || opcao[0] == '-1');
+    } while (opcao[0] == '4' || opcao[0] == '-1');
 
 }
 
 int main() {
-	int max_cadastros = 100;
-	Conta *Contas = new Conta[max_cadastros];
-	int qtd_cadastros = 0;
-	menu_login(Contas, qtd_cadastros);
-	listar_contas(Contas, qtd_cadastros);
-	delete[] Contas;
-	return 0;
+    int max_cadastros = 100;
+    Conta *Contas = new Conta[max_cadastros];
+    int qtd_cadastros = 0;
+    menu_login(Contas, qtd_cadastros);
+    listar_contas(Contas, qtd_cadastros);
+    menu_login(Contas, qtd_cadastros);
+    delete[] Contas;
+    return 0;
 }
