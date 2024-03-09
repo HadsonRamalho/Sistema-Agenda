b2615fa2db44445e2aa9d8e434746d3b8bd8767b
Hadson <hadsonramalho@gmail.com>
2024-03-09 08:33:13 -0300

    Protótipo de inicialização

--- Sistema Agenda/Sistema Agenda/Dev-Dados.h
+++ Sistema Agenda/Sistema Agenda/Dev-Dados.h
@@ -7,3 +7,4 @@ void inicializa_nomes(std::vector<Conta>& Conta, int max_inicializacoes);
 void le_dados(std::vector<Conta>& Conta, int max_inicializacoes);
 void escreve_dados(std::vector<Conta>& Conta, int max_inicializacoes);
 void inicia_dados(std::vector<Conta> &Contas, int max_inicializacoes);
+void inicializa_empresas(std::vector<Conta>& Conta_Param, int max_inicializacoes);
