1cc97fd8dd913160b3cc4e8ef882fe05a9e08d81
Hadson <hadsonramalho@gmail.com>
2024-03-09 07:42:25 -0300

    Protótipos adaptados

--- Sistema Agenda/Sistema Agenda/Dev-Dados.h
+++ Sistema Agenda/Sistema Agenda/Dev-Dados.h
@@ -2,8 +2,8 @@
 #include <iostream>
 #include "Classes.h"
 
-void exibe_dados(std::vector<Conta_Comum> Conta, int max_inicializacoes);
-void inicializa_nomes(std::vector<Conta_Comum>& Conta, int max_inicializacoes);
-void le_dados(std::vector<Conta_Comum>& Conta, int max_inicializacoes);
-void escreve_dados(std::vector<Conta_Comum>& Conta, int max_inicializacoes);
-void inicia_dados(int max_inicializacoes);
+void exibe_dados(std::vector<Conta> Conta, int max_inicializacoes);
+void inicializa_nomes(std::vector<Conta>& Conta, int max_inicializacoes);
+void le_dados(std::vector<Conta>& Conta, int max_inicializacoes);
+void escreve_dados(std::vector<Conta>& Conta, int max_inicializacoes);
+void inicia_dados(std::vector<Conta> &Contas, int max_inicializacoes);
